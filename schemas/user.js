6a94d6c57e457cfb779617d047478036e7d02ca7
Ankur Mungekar <ankur.m@inmu-ankur.m-211013>
2022-04-07 22:08:25 +0530

    added files

---
+++ schemas/user.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UserName',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      }
+    }
+  ],
+};
